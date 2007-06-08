04a633e3488941a30edb1ae3fe6c93eb8758b4d1
Alex Sidorenko <asid@hp.com>
2007-06-08 14:42:17 +0000

    Rewriting the parser to use regexps without pyparsing

--- pykdump/BTstack.py
+++ pykdump/BTstack.py
@@ -19,9 +19,10 @@ from 'bt' command. At this moment we are just parsing results (text) obtained
 by running 'bt', later we might switch to something better.
 '''
 
-from pykdump.API import *
+#from pykdump.API import *
 
 import string
+import time, os
 
 from pyparsing import *
 import pyparsing as pyp
@@ -67,10 +68,14 @@ FRAME_start = Suppress("#") + intval + \
               Optional("(via" +  SkipTo(")", include=True)).suppress() + \
               Suppress("at") + noprefix_hexval
 
+FRAME_start = Regex("\s*#(\d+)\s+\[([^\]]+)\] .+$", re.M)
 FRAME_empty = Suppress('(active)')
 
 REG_context = Suppress(SkipTo(Literal('#') | Literal("PID")))
+#REG_context = Regex("  \s+[^#]+$", re.M)
 FRAME = (FRAME_start | FRAME_empty) + Optional(REG_context)
+
+#FRAME = FRAME_start + ZeroOrMore(REG_context)
 
 PID = PID_line + Group(OneOrMore(Group(FRAME)))
 PIDs = OneOrMore(Group(PID))
@@ -92,7 +97,13 @@ class BTFrame:
     def __init__(self):
         pass
     def __repr__(self):
-        return "  #%-2d  %s+0x%x" % (self.level, self.func, self.offset)
+        if (self.data):
+            datalen = len(string.join(self.data))
+            data = ', %d bytes of data' % datalen
+        else:
+            data = ''
+        return "  #%-2d  %s+0x%x%s" % \
+               (self.level, self.func, self.offset, data)
 
 
 import pprint
@@ -105,6 +116,15 @@ def exec_bt(cmd = None, text = None):
         # Execute a crash command...
         text = exec_crash_command(cmd)
         #print text
+
+    t0 = os.times()[0]
+    res =  PIDs.parseString(text).asList()
+    t1 = os.times()[0]
+
+    pp.pprint(res)
+    print "%7.2f s to parse" % (t1 - t0)
+    return
+
     for pid, task, cpu, cmd, finfo in PIDs.parseString(text).asList():
         bts = BTStack()
         bts.pid = pid
@@ -121,3 +141,73 @@ def exec_bt(cmd = None, text = None):
             bts.frames.append(f)
         pp.pprint(bts)
     
+
+# A parser using regular expressions only - no pyparsing
+# PID: 0      TASK: c55c10b0  CPU: 1   COMMAND: "swapper"
+re_pid = re.compile(r'^PID:\s+(\d+)\s+TASK:\s+([\da-f]+)\s+' +
+                    'CPU:\s(\d+)\s+COMMAND:\s+"([^"]+)".*$')
+
+# Frame start can have one of three forms:
+#  #0 [c038ffa4] smp_call_function_interrupt at c0116c4a
+# #7 [f2035f20] error_code (via page_fault) at c02d1ba9
+# (active)
+
+re_f1 = re.compile(r'\s*#(\d+)\s+\[([\da-f]+)\]\s+(.+)\sat\s([\da-f]+)$')
+re_via = re.compile(r'(\S+)\s+\(via\s+([^)]+)\)$')
+
+def exec_bt_new(cmd = None, text = None):
+    # Debugging
+    if (cmd != None):
+        # Execute a crash command...
+        text = exec_crash_command(cmd)
+        #print text
+
+    t0 = os.times()[0]
+
+    # Split text into one-thread chunks
+    for s in text.split("\n\n"):
+        print '-' * 50
+        #print s
+        # The first line is PID-line, after that we have frames-list
+        lines = s.splitlines()
+        pidline = lines[0]
+        print pidline
+        m = re_pid.match(pidline)
+        pid = int(m.group(1))
+        addr = int(m.group(2), 16)
+        cpu = int(m.group(3))
+        cmd = m.group(4)
+
+        bts = BTStack()
+        bts.pid = pid
+        bts.cmd = cmd
+        bts.frames = []
+
+        print "%d 0x%x %d <%s>" % (pid, addr, cpu, cmd)
+        f = None
+        for fl in lines[1:]:
+            m = re_f1.match(fl)
+            #print '--', fl
+            if (m):
+                f = BTFrame()
+                f.level = int(m.group(1))
+                f.func = m.group(3)
+                viam = re_via.match(f.func)
+                if (viam):
+                    f.func = viam.group(2)
+                # If we have a pattern like 'error_code (via page_fault)'
+                # it makes more sense to use 'via' func as a name
+                f.addr = int(m.group(4), 16)
+                f.offset = f.addr
+                f.data = []
+                bts.frames.append(f)
+            elif (f != None):
+                f.data.append(fl)
+
+        t1 = os.times()[0]
+        pp.pprint(bts) 
+                
+    print "%7.2f s to parse" % (t1 - t0)
+
+
+exec_bt = exec_bt_new        
