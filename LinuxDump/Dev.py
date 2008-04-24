d757b2ec623df06e4357f186e555ba07b1005b94
Alex Sidorenko <asid@hp.com>
2008-04-24 20:13:19 +0000

    Adding a library module for block devices

---
+++ LinuxDump/Dev.py
@@ -0,0 +1,21 @@
+# module LinuxDump.Dev
+#
+# Time-stamp: <08/03/05 15:51:52 alexs>
+#
+# Copyright (C) 2008 Alex Sidorenko <asid@hp.com>
+# Copyright (C) 2008 Hewlett-Packard Co., All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+__doc__ = '''
+This is a package providing generic access to block devices
+'''
+
