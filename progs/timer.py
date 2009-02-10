1e35cc2f6efa9737e76cff03fbd594bf89b82364
Alex Sidorenko <asid@hp.com>
2009-02-10 16:49:29 +0000

    Pykdump version of 'timer' command to print more details about corruption patterns

---
+++ progs/timer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Time-stamp: <09/01/22 10:29:33 alexs>
+
+# Copyright (C) 2006 Alex Sidorenko <asid@hp.com>
+# Copyright (C) 2006 Hewlett-Packard Co., All rights reserved.
+
+# Print info about connections and sockets 
+
+from pykdump.API import *
+from LinuxDump import percpu
+
+# Analyzing timer lists
+__TIMER_MAGIC = 0x4b87ad6e
+for cpu, a in  enumerate(percpu.get_cpu_var("tvec_bases")):
+    s = readSU("struct tvec_t_base_s", a)
+    print "---CPU#%d" % cpu, s
+    # tv1-tv5 are arrays of list_heads
+    for tvi in range(1,6):
+	sn = "tv%d" % tvi
+	tv = s.__getattr__(sn)
+	print "  ", sn, tv
+	for i, lh in enumerate(tv.vec):
+	    lst, err = readBadList(lh, inchead = False, maxel=10000)
+	    if (len(lst)):
+	       print "\t", i, lh, len(lst)
+	    if (err):
+		print "\t  +++ %s.vec[%d]. lhaddr=0x%x" % (sn, i, long(lh)), err
+		# Strip the bad element
+		lst = lst[:-1]
+	    # Print the list
+	    for a in lst:
+		tl = readSU("struct timer_list", a)
+		if (tl.magic != __TIMER_MAGIC):
+		    bm = "bad magic"
+		else:
+		    bm = addr2sym(tl.function)
+		print "\t  ", tl, bm
+
