3424845ba00df3f727ce6f361effd52c811dc4b5
Alex Sidorenko <asid@hp.com>
2015-10-07 15:30:23 -0400

    Add seconds_since_boot() and other similar subroutines

--- LinuxDump/Time.py
+++ LinuxDump/Time.py
@@ -3,9 +3,9 @@
 # module LinuxDump.Time
 #
 # --------------------------------------------------------------------
-# (C) Copyright 2014 Hewlett-Packard Development Company, L.P.
+# (C) Copyright 2014-2015 Hewlett-Packard Development Company, L.P.
 #
-# Author: Alex Sidorenko <asid@hp.com>
+# Author: Alex Sidorenko <asid@hpe.com>
 #
 # --------------------------------------------------------------------
 # This program is free software; you can redistribute it and/or modify
@@ -24,9 +24,15 @@ __doc__ = '''
 This is a package providing subroutines for time manipulation
 '''
 
+__all__ = ['j_delay', 'seconds_since_boot', 'get_uptime_fromproc',
+           'get_uptime_fromcrash']
+
 # Tasks and Pids
 
 from pykdump.API import *
+import crash
+from collections import namedtuple
+
 
 def j_delay(ts, jiffies):
     v = (jiffies - ts) & INT_MASK
@@ -37,3 +43,68 @@ def j_delay(ts, jiffies):
     else:
         v = "%8.2f s" % (float(v)/HZ)
     return v
+
+class _readSymbol():
+    def __init__(self, topsymbol = None):
+        self.topsymbol = topsymbol
+    def __getattr__(self, attr):
+        if (self.topsymbol):
+            return getattr(readSymbol(self.topsymbol), attr)
+        else:
+            return readSymbol(attr)
+
+timespec = namedtuple("timespec", ['tv_sec', 'tv_nsec'])
+
+NSEC_PER_SEC = 1000000000
+def set_normalized_timespec(sec, nsec):
+    while (nsec >= NSEC_PER_SEC):
+        nsec -= NSEC_PER_SEC
+        sec += 1
+	
+    while (nsec < 0):
+        nsec += NSEC_PER_SEC
+        sec -= 1
+
+    return timespec(sec, nsec)
+
+
+
+def getboottime():
+    tv_sec = tk.wall_to_monotonic.tv_sec +  tk.total_sleep_time.tv_sec
+    tv_nsec = tk.wall_to_monotonic.tv_nsec + tk.total_sleep_time.tv_nsec
+    return set_normalized_timespec(-tv_sec, -tv_nsec)
+    
+
+def seconds_since_boot():
+    boot = getboottime()
+    return get_seconds() - boot.tv_sec
+
+# Kernel-dependent subroutines. In older kernels, we have a global
+# 'struct timespec wall_to_monotonic' and 'xtime', on newer kernels they
+# are members, 'timekeeper.wall_to_monotonic' and 'timekeeper.xtime'
+#
+
+
+if (symbol_exists("wall_to_monotonic")):
+    tk = _readSymbol()
+    def get_seconds():
+        return tk.xtime.tv_sec
+    # In old (e.g. RHEL5) kernels, there is no seconds_since_boot().
+    # In this case, refage calculations in
+    # use just get_seconds() -> xtime.tv_sec;
+    if (not symbol_exists("total_sleep_time")):
+        seconds_since_boot = None
+else:
+    tk = _readSymbol("timekeeper")
+    def get_seconds():
+        return tk.xtime_sec
+        
+
+def get_uptime_fromproc():
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = int(float((f.readline().split()[0])))
+    return uptime_seconds
+
+
+def get_uptime_fromcrash():
+    return crash.get_uptime()/HZ
