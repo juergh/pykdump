7afcaaad2982168eef32f9909090deb12d744dcf
Alex Sidorenko <asid@hpe.com>
2015-11-25 14:40:29 -0500

    Major improvements both in speed and number of tests

--- progs/hanginfo.py
+++ progs/hanginfo.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # --------------------------------------------------------------------
-# (C) Copyright 2006-2015 Hewlett-Packard Development Company, L.P.
+# (C) Copyright 2006-2015 Hewlett Packard Enterprise Development LP
 #
-# Author: Alex Sidorenko <asid@hp.com>
+# Author: Alex Sidorenko <asid@hpe.com>
 #
 # --------------------------------------------------------------------
 #  Analyze reasons why some tasks are hanging (TASK_UNINTERRUPTIBLE)
@@ -12,9 +12,10 @@
 # To facilitate migration to Python-3, we start from using future statements/builtins
 from __future__ import print_function
 
-__version__ = "0.2"
+__version__ = "0.3"
 
 import sys
+import re
 import textwrap
 import operator
 
@@ -25,60 +26,47 @@ from pykdump.API import *
 from pykdump.Generic import Bunch
 
 from LinuxDump import percpu
-from LinuxDump.Tasks import TaskTable, Task, tasksSummary, ms2uptime, decode_tflags, decode_waitq
-from LinuxDump.BTstack import exec_bt, bt_summarize, bt_mergestacks
+from LinuxDump.Tasks import (TaskTable, Task, tasksSummary, ms2uptime, decode_tflags,
+                             decode_waitq, TASK_STATE)
+from LinuxDump.BTstack import (exec_bt, bt_mergestacks,
+                               get_threads_subroutines, verifyFastSet)
 from LinuxDump.inet import proto
 from LinuxDump.Analysis import print_wait_for_AF_UNIX
+from LinuxDump.Files import pidFiles
 
 from collections import namedtuple, defaultdict
 
 debug = API_options.debug
 
+# Resource owners - that is, threads that own some mutex/sema etc. that
+# other threads are waiting on
+
+__resource_owners = set()
+
+# Print header: what we are waiting for and number of pids
+def print_header(msg, n = None):
+    print("\n {:=^50}".format(" " + msg + " "))
+
 # Get UNINTERRUPTIBLE tasks
-# In reality, get STOPPED, TRACED, ZOMBIE, DEAD and WAKING etc too - everygthing > 1
+# In reality, get STOPPED, TRACED, ZOMBIE, DEAD and WAKING etc too - 
+# everygthing > 1
+# Return a set of pids/tids
 #@memoize_cond(CU_LIVE)
 def getUNTasks():
-    basems = None
-    #quit()
-    tt = TaskTable()
-    if (debug):
-        print ("Basems", tt.basems, "Uptime:",  ms2uptime(tt.basems))
-    
+    return {t.pid for t in T_table.allThreads() \
+        if t.ts.state & TASK_STATE.TASK_UNINTERRUPTIBLE}
 
-    out = []
-    basems = tt.basems
-    
-
-    # Most recent first
-    for t in tt.allThreads():
-        out.append((basems - t.Last_ran, t.pid, t))
-    out.sort()
-
-    outUN = {}
-    #print (" PID          CMD       CPU   Ran ms ago   STATE")
-    #print ("--------   ------------  --  ------------- -----")
-
-    for ran_ms_ago, pid, t in out:
-        #sstate = t.state[5:7]
-        #if (sstate != 'UN'):
-        #    continue
-        if (t.ts.state < 2):
-            continue
-        
-        b = Bunch()
-        b.ran_ms_ago = ran_ms_ago
-        bt = exec_bt("bt %d" % pid)[0]
-        b.bt = bt
-        outUN[pid] = b
-    return outUN
+# Remove a list of pids from the provided dictionary
+def remove_pidlist(pset, pids):
+    pset -= set(pids)
 
 # Print pidlist according to verbosity setting
-def print_pidlist(pids, tasksref):
+def print_pidlist(pids):
     # Prepare a list of (ran_ms_ago, pid) list to sort them
     mlist = []
     for pid in pids:
-        b = tasksref[pid]
-        mlist.append((b.ran_ms_ago, pid))
+        t = T_table.getByTid(pid)
+        mlist.append((t.Ran_ago, pid))
     mlist = sorted(mlist)
     if (verbose):
         print("        PID     Ran ms ago")
@@ -92,53 +80,6 @@ def print_pidlist(pids, tasksref):
         print("    Sorted pids (youngest first) [%d, %d] ms ago" % \
             (ran_y, ran_o))
         print(textwrap.fill(str(spids), initial_indent=' '*6, subsequent_indent=' ' *7))
-        
-def check_all_tasks():
-    lines = memoize_cond(CU_LIVE)(exec_crash_command)("ps")
-    waiters = {}
-    for l in lines.splitlines()[1:]:
-        fields = l[1:].split()
-        task = readSU("struct task_struct", int(fields[3], 16))
-        if (not task.mm or not task.mm.mmap_sem):
-            continue
-        s = task.mm.mmap_sem
-        #wait_list elements are embedded in struct rwsem_waiter
-        wait_list = readSUListFromHead(Addr(s.wait_list), "list",
-                "struct rwsem_waiter")
-        pids = [w.task.pid for w in wait_list]
-        if (pids):
-            waiters[s] = pids
-    return waiters
-        
-
-def check_all_files():
-    lines = memoize_cond(CU_LIVE)(exec_crash_command)("foreach files")
-    inode_addrs = {}
-    for l in lines.splitlines():
-        fields = l.split()
-        if (len(fields) < 5 or fields[4] != 'REG'):
-            continue
-        if (len(fields) == 5):
-            fields.append("")
-
-        inode_addrs[int(fields[3], 16)] = fields[5]
-    
-    #print(len(inode_addrs))
-    waiters = {}
-    for inodeaddr, v in inode_addrs.items():
-        inode = readSU("struct inode", inodeaddr)
-        #print(hex(inode), v)
-        # 1: unlocked, 0: locked, negative: locked, possible waiters
-        counter = inode.i_mutex.count.counter
-        if (counter >= 0):
-            continue
-        mutex = inode.i_mutex
-        wait_list = readSUListFromHead(Addr(mutex.wait_list), "list",
-             "struct mutex_waiter")
-        pids = [w.task.pid for w in wait_list]
-        waiters[inode] = (v, pids)
-
-    return waiters
 
 # A test to check heuristically whether a pointer looks like mutex
 # On RHEL6 and later
@@ -172,43 +113,6 @@ def if_mutexOK(addr):
     except:
         return 0
 
-__mutexfunc = "__mutex_lock_slowpath"
-def check_mutex_lock(tasksref, tasksrem):
-    mutexlist = set()
-    for pid in sorted(tasksref.keys()):
-        bt = tasksref[pid].bt
-        frames = bt.frames
-        if (not bt.hasfunc(__mutexfunc)):
-            continue
-        #print(bt)
-        b1 = exec_bt("bt -f {}".format(pid))[0]
-        for f in b1.frames:
-            if (f.func.find(__mutexfunc) != -1):
-                addrs = __stackdata2array(f.data)
-                l_addrs = len(addrs)
-                for pos in range(6,9):
-                    if (pos >= l_addrs):
-                        continue
-                    maddr = addrs[pos]
-                    if (if_mutexOK(maddr)):
-                        mutex = readSU("struct mutex", maddr)
-                        mutexlist.add(mutex)
-                        continue
-
-    if (not mutexlist):
-        return
-    # Now print info about each found mutex
-    print(" === Waiting on mutexes =========")
-    for mutex in mutexlist:
-        owner = mutex.Owner
-        pids = get_mutex_waiters(mutex)
-        if (owner and not pids):
-            continue
-        print_mutex(mutex)
-        print_pidlist(pids, tasksref)
-        remove_pidlist(tasksrem, pids)
-
-
 # Get a list of pids waiting on mutex
 def get_mutex_waiters(mutex):
     if (mutex.Owner and mutex.count.counter >= 0):
@@ -231,9 +135,11 @@ def print_mutex(mutex):
     # Try to get the owner
     if (mutex.hasField("owner") and mutex.owner):
         ownertask = mutex.Owner
+        ownerpid = ownertask.pid
     else:
         ownertask = ''
     if (ownertask):
+        __resource_owners.add(ownerpid)
         ownertask = "\n   Owner: pid={0.pid} cmd={0.comm}".format(ownertask)
     else:
         ownertask = "\n   No Owner"
@@ -254,15 +160,7 @@ def __stackdata2array(lines):
             arr.append(int(d, 16))
     return arr
 
-# Remove a list of pids from the provided dictionary
 
-def remove_pidlist(tdict, pids):
-    for pid in pids:
-        try:
-            del tdict[pid]
-        except KeyError:
-            pass
-    
 # Print backing_dev_info
 def print_backing_dev_info(bdi):
     work_list = bdi.work_list
@@ -278,44 +176,180 @@ def print_backing_dev_info(bdi):
 def print_bdi_writeback(wb):
     for inode in ListHead(wb.b_io.prev, 'struct inode').i_list:
         print(inode, inode.i_state, inode.i_mapping)
-        
 
-# Classify UNINTERRUPTIBLE threads
-def classify_UN(v):
-    # This is a reference list - we do not touch it
-    tasksref = getUNTasks()
-    
-    # And this is a copy - we will be removing from it pids if we understand
-    # what they are waiting for. In the end, only those pids that cannot be
-    # categorized will be left here
-    tasksrem = tasksref.copy()
+# Special tests for HANA-hangs. When we are out of memory, we are spinning
+# trying to shrink VM zones and can hold mmap semaphores
+# Find all do_page_fault threads that are RUNNABLE
+__pagefaultfunc = "do_page_fault"
+def find_pagefault():
+    tt = T_table
+    out = defaultdict(list)
+    goodpids = funcpids[__pagefaultfunc]
+    #print(goodpids)
+    for t in tt.allThreads():
+        if (not t.pid in goodpids):
+            continue
+        if (t.ts.state in (TASK_STATE.TASK_RUNNING, TASK_STATE.TASK_UNINTERRUPTIBLE)):
+            #bt = exec_bt("bt %d" % t.pid)[0]
+            #if (not bt.hasfunc(__pagefaultfunc)):
+            out[t.mm.mmap_sem].append(t.pid)
+    return out
 
-    if (not tasksref):
-        print ("There are no UNINTERRUPTIBLE tasks")
+# Print statistics for threads trying to srhink zones. 
+__shrinkfunc = "shrink_all_zones"
+def find_shrinkzones():
+    shrinkpids = funcpids[__shrinkfunc]
+    # we do not need to be 100% sure, this is just for information
+    #verifyFastSet(shrinkpids, __shrinkfunc)
+    if (not shrinkpids):
         return
+    d = defaultdict(int)
+    total = 0
+    for pid in shrinkpids:
+        t = T_table.getByTid(pid)
+        d[t.state] += 1
+        total += 1
+    # Print
+    print_header(" There are {} threads doing {}".format(total, __shrinkfunc))
+    for k,v in d.items():
+        print("  {:3d} in {}".format(v, k))
 
-    print (" *** UNINTERRUPTIBLE threads, classified ***")
-    # Fin mutexes we are waiting on
-    # Stage one - analyze global structures
-    mutex_waiters = check_all_files()
+
+# Find all PIDs that match a give mm,mmap_sem address
+def find_pids_mmap(addr):
+    tt = T_table
+    for t in tt.allThreads():
+        if (long(t.mm.mmap_sem) == long(addr)):
+            print(t.pid)
+
+#
+# ============= Check subroutines =======================
+#
+def check_mmap_sem(tasksrem):
+    waiters = {}
+    for addr in alltaskaddrs:
+        task = readSU("struct task_struct", addr)
+        if (not task.mm or not task.mm.mmap_sem):
+            continue
+        s = task.mm.mmap_sem
+        #wait_list elements are embedded in struct rwsem_waiter
+        wait_list = readSUListFromHead(Addr(s.wait_list), "list",
+                "struct rwsem_waiter")
+        pids = [w.task.pid for w in wait_list]
+        if (pids):
+            waiters[s] = pids
+
+    mmapsem_waiters = waiters
+    possible_owners = find_pagefault()
+    if (mmapsem_waiters):
+        print_header("Waiting on mmap semaphores")
+        for sema, pids in mmapsem_waiters.items():
+            print ("  --", sema)
+            print_pidlist(pids)
+            powners = possible_owners.get(sema, None)
+            if (powners):
+                pid_set = set(pids)
+                powners = set(powners)
+                # Leave only those powners that are not in pids
+                rem = powners - pid_set
+                print("        Possible owners:", list(rem))
+                for o in rem:
+                    __resource_owners.add(o)
+            remove_pidlist(tasksrem, pids)
+
+      
+
+def check_inode_mutexes(tasksrem):
+    goodpids = funcpids[__mutexfunc] | \
+        funcpids["__mutex_lock_killable_slowpath"]
+    inode_addrs = {}
+    for pid in goodpids:
+        finfo = pidFiles(pid).files
+        for fd in finfo:
+            fields = finfo[fd]
+            inode_addr = fields[2]
+            ftype = fields[3]
+            path = fields[4]
+            if (ftype not in ('REG', 'DIR', 'PIPE')):
+                continue
+    
+            inode_addrs[inode_addr] = path
+    
+    #print(len(inode_addrs))
+    waiters = {}
+    for inodeaddr, v in inode_addrs.items():
+        inode = readSU("struct inode", inodeaddr)
+        #print(hex(inode), v)
+        # 1: unlocked, 0: locked, negative: locked, possible waiters
+        counter = inode.i_mutex.count.counter
+        if (counter >= 0):
+            continue
+        mutex = inode.i_mutex
+        wait_list = readSUListFromHead(Addr(mutex.wait_list), "list",
+             "struct mutex_waiter")
+        pids = [w.task.pid for w in wait_list]
+        waiters[inode] = (v, pids)
+
+    mutex_waiters = waiters
     if (mutex_waiters):
-        print(" === Waiting on inode mutexes ==========")
+        print_header("Waiting on inode mutexes")
         for inode, (fn, pids) in mutex_waiters.items():
             print ("  --", inode, fn)
             print_mutex(inode.i_mutex)
-            print_pidlist(pids, tasksref)
+            print_pidlist(pids)
             remove_pidlist(tasksrem, pids)
 
-    check_mutex_lock(tasksref, tasksrem)
-   
-    mmapsem_waiters = check_all_tasks()
-    if (mmapsem_waiters):
-        print(" === Waiting on mmap sempahores =========")
-        for sema, pids in mmapsem_waiters.items():
-            print ("  --", sema)
-            print_pidlist(pids, tasksref)
-            remove_pidlist(tasksrem, pids)
 
+# Check cred_guard_mutex. 
+# Older kernels: this is task.cred_guard_mutex
+# Newer kernels: this is task.signal.cred_guard_mutex
+structSetAttr("struct task_struct", "Cred_guard_mutex", 
+              ["cred_guard_mutex", "signal.cred_guard_mutex"])
+
+def check_cred_guard_mutex(task):
+    gm = task.Cred_guard_mutex
+    return gm
+    
+__mutexfunc = "__mutex_lock_slowpath"
+def check_other_mutexes(tasksrem):
+    #print(mutexlist)
+    mutexlist = set()
+    goodpids = funcpids[__mutexfunc]
+    for pid in tasksrem:
+        if (not pid in goodpids):
+        #if (not bt.hasfunc(__mutexfunc)):
+            continue
+        #print(bt)
+        b1 = exec_bt("bt -f {}".format(pid))[0]
+        for f in b1.frames:
+            if (f.func.find(__mutexfunc) != -1):
+                addrs = __stackdata2array(f.data)
+                l_addrs = len(addrs)
+                for pos in range(6,9):
+                    if (pos >= l_addrs):
+                        continue
+                    maddr = addrs[pos]
+                    if (if_mutexOK(maddr)):
+                        mutex = readSU("struct mutex", maddr)
+                        mutexlist.add(mutex)
+                        continue
+
+    if (not mutexlist):
+        return
+    # Now print info about each found mutex
+    once = TrueOnce(1)
+    for mutex in mutexlist:
+        owner = mutex.Owner
+        pids = get_mutex_waiters(mutex)
+        if (owner and not pids):
+            continue
+        if (once):
+            print_header("Waiting on mutexes")
+        print_mutex(mutex)
+        print_pidlist(pids)
+        remove_pidlist(tasksrem, pids)
+
+def check_congestion_queues(tasksrem):
     # Congestion queues    
     for i, wqh in enumerate(readSymbol("congestion_wqh")):
         pids = []
@@ -335,17 +369,66 @@ def classify_UN(v):
                 print(" ---- waiting on the write congestion queue ---")
                 print(textwrap.fill(str(pids), initial_indent=' '*3, subsequent_indent=' ' *4))
                 remove_pidlist(tasksrem, pids)
-        
+
+# Check and print based on functions on the stack
+def check_stack_and_print(funcname, tasksrem):
+    pids = funcpids[funcname] & tasksrem
+    verifyFastSet(pids, funcname)
+    if (not pids):
+        return
+    print_header("Waiting in {}".format(funcname))
+    print_pidlist(pids)
+    remove_pidlist(tasksrem, pids)
+
+
+# ==============end of check subroutines=======================   
+       
+
+# Classify UNINTERRUPTIBLE threads
+def classify_UN(v):
+    # Reset owners
+    __resource_owners.clear()
+    # We get a list of UN tasks
+    tasksrem = getUNTasks()
+    
+    if (not tasksrem):
+        print ("There are no UNINTERRUPTIBLE tasks")
+        return
+
+    print (" *** UNINTERRUPTIBLE threads, classified ***")
+
+    # Now we are do a number of tests trying to classify the threads
+    # Every time we succeed, we remove these threads from tasksrem
+    check_stack_and_print('io_schedule', tasksrem)
+    check_stack_and_print('btrfs_tree_read_lock', tasksrem)
+    check_inode_mutexes(tasksrem)
+    check_other_mutexes(tasksrem)
+    check_mmap_sem(tasksrem)
+    check_congestion_queues(tasksrem)
+
+    
     un = tasksrem
     if (not un):
         return
 
-    print ("\n\n ********  Non-classified UN Threads **********")
+    print ("\n\n ********  Non-classified UN Threads ********** {} in total".
+           format(len(un)))
     
     # Print what remains
-    btlist = [b.bt for b in un.values()]
+    btlist = [exec_bt("bt %d" % pid)[0] for pid in un]
     bt_mergestacks(btlist, verbose=1)
     #print(un)
+    # Print resource owners
+    if (__resource_owners):
+        print("\n*** Threads that own resources the other threads are"
+            " waiting on ***")
+        for pid in __resource_owners:
+            s = exec_bt("bt {}".format(pid))[0]
+            print(s)
+    # Are any of these owners looping in zone allocator?
+    #_owners = zvm_pids & rem
+    #if (_owners):
+    #    print("        Looping in zone allocator:", list(_owners))
     return
 
     # The following code is not ready yet
@@ -379,6 +462,7 @@ if ( __name__ == '__main__'):
     (o, args) = op.parse_args()
     
     v = verbose = o.Verbose
+    T_table = TaskTable()
     if (o.Version):
         print ("HANGINFO version %s" % (__version__))
         sys.exit(0)
@@ -386,8 +470,19 @@ if ( __name__ == '__main__'):
     if (o.Syslogger):
         print_wait_for_AF_UNIX(verbose)
         sys.exit(0)
-        
+     
+    funcpids, functasks, alltaskaddrs  = get_threads_subroutines()
+    #find_shrinkzones()
+    #for pid in funcsMatch(funcpids, r'^io_schedule'):
+    #    print(pid)
+    
+    #sys.exit(0)    
+    zvm_pids = set(funcpids[__shrinkfunc])
+
+    #find_pids_mmap(0xffff8b1b7c858ee0)
+    #sys.exit(0)
     classify_UN(v)
+    find_shrinkzones()
     
     print("\n")
     print_wait_for_AF_UNIX(0)
