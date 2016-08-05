a2c4858dc74360b6c4caa91745867eeb06280a63
Alex Sidorenko <asid@hpe.com>
2016-08-05 14:40:37 -0400

    Added emaulation of PCI info

--- LinuxDump/pci.py
+++ LinuxDump/pci.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # --------------------------------------------------------------------
-# (C) Copyright 2006-2015 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2006-2016 Hewlett Packard Enterprise Development LP
 #
 # Author: Alex Sidorenko <asid@hpe.com>
 #
@@ -15,6 +15,8 @@ from pykdump.API import *
 
 from LinuxDump.sysfs import *
 from LinuxDump.kobjects import *
+
+__all__ = ["print_PCI_devices", "print_PCI_resources"]
 
 #static inline const char *pci_name(const struct pci_dev *pdev)
 #{
@@ -61,7 +63,69 @@ def print_PCI_devices(v = 0):
         print("{} {:04x}: {:04x}:{:04x} (rev {:02x})".\
               format(name[-7:], pclass >> 8,
                      vendor, device, revision))
+ 
+#static struct resource *next_resource(struct resource *p, bool sibling_only)
+#{
+        #/* Caller wants to traverse through siblings only */
+        #if (sibling_only)
+                #return p->sibling;
+
+        #if (p->child)
+                #return p->child;
+        #while (!p->sibling && p->parent)
+                #p = p->parent;
+        #return p->sibling;
+#}
+
+def next_resource(p):
+    if (p.child):
+        return p.child
+
+    while (not p.sibling and p.parent):
+        p = p.parent
+
+    return p.sibling
+
+_MAX_IORES_LEVEL = enumerator_value("MAX_IORES_LEVEL")
+
+def walk_resource(root, v = 0):
+    res = root.child
+    #int width = root->end < 0x10000 ? 4 : 8;
+    width = 4 if root.end < 0x10000 else 8
     
+    fmt = "{:0>%dx}-{:0>%dx} : {}" % (width, width)
+    n = 0
+    while (True):
+        #for(depth = 0, p = r; depth < MAX_IORES_LEVEL; depth++, p = p->parent)
+        #        if (p->parent == root)
+        #                break;
+        p = res
+        for depth in range(_MAX_IORES_LEVEL):
+            if (p.parent == root):
+                break
+            p = p.parent
+        padding = '  ' * depth
+        
+        #print(res)
+        if (v):
+            # Print structure addr
+            print("\n{:-^40}".format(str(res)))
+        print(padding, fmt.format(res.start, res.end, res.name), sep='')
+        n += 1
+        res =  next_resource(res)
+        if (not res):
+            break
+    #print("\n {} total".format(n))
+
+def print_PCI_resources(v = 0):
+    for resname in ("iomem_resource", "ioport_resource"):
+        try:
+            root = readSymbol(resname)
+        except:
+            continue
+        print("\n{:=^70}".format(resname))
+        walk_resource(root, v)
+
 if ( __name__ == '__main__'):
     import sys
     import argparse
